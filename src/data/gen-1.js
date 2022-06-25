bec600e6bb89575d3c2a9ccbf86b00f2ce181d96
Luc cole <luccoleb@yahoo.com>
2022-06-25 11:36:32 -0400

    Gen 1 data

---
+++ src/data/gen-1.js
@@ -0,0 +1,761 @@
+
+const gen1 = [
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 1
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 2
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 3
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 4
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 5
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 6
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 7
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 8
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 9
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 10
+  },
+
+
+
+
+
+
+
+
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 11
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 12
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 13
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 14
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 15
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 16
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 17
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 18
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 19
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 20
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 21
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 22
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 23
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 24
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 25
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 26
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 27
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 28
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 29
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 30
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 31
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 32
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 33
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 34
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 35
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 36
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 37
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 38
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 39
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 40
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 41
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 42
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 43
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 44
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 45
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 46
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 47
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 48
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 49
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 50
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 51
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 52
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 53
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 54
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 55
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 56
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 57
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 58
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 59
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 60
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 61
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 62
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 63
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 64
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 65
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 66
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 67
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 68
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 69
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 70
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 71
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 72
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 73
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 74
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 75
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 76
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 77
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 78
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 79
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 80
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 81
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 82
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 83
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 84
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 85
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 86
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 87
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 88
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 89
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 90
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 91
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 92
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 93
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 94
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 95
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 96
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 97
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 98
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 99
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 100
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 101
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 102
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 103
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 104
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 105
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 106
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 107
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 108
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 109
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 110
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 111
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 112
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 113
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 114
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 115
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 116
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 117
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 118
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 119
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 120
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 121
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 122
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 123
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 124
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 125
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 126
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 127
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 128
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 129
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 130
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 131
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 132
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 133
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 134
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 135
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 136
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 137
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 138
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 139
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 140
+  },
+  {
+    name: 'bulbasaur',
+    found: true,
+    id: 141
+  },
+  {
+    name: 'ivysaur',
+    found: true,
+    id: 142
+  },
+  {
+    name: 'venusaur',
+    found: true,
+    id: 143
+  },
+  {
+    name: 'charmander',
+    found: true,
+    id: 144
+  },
+  {
+    name: 'charmeleon',
+    found: true,
+    id: 145
+  },
+  {
+    name: 'charizard',
+    found: true,
+    id: 146
+  },
+  {
+    name: 'squirtle',
+    found: true,
+    id: 147
+  },
+  {
+    name: 'wartortle',
+    found: true,
+    id: 148
+  },
+  {
+    name: 'blastoise',
+    found: true,
+    id: 149
+  },
+  {
+    name: 'caterpie',
+    found: true,
+    id: 150
+  }
+];
